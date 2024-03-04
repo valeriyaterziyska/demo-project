3c70ad7501ee70360eb9b490bb4f44422868171b
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-03-04 19:17:58 +0200

    basic component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nav',
+    template: '<div id="nav-wrapper">Hello navigation</div>',
+    styles: ['#nav-wrapper: {background-color: pink}']
+})
+
+export class NavComponent { }
