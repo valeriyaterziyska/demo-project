ba705d71750c26aea72ab77489b7b645be9b14f5
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-03-11 12:33:50 +0200

    basic user list

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+type User = {
+  name: String,
+  age: Number,
+}
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
-
-}
+  users = [
+    { name: 'Pesho', age: 18 },
+    { name: 'Mitko', age: 24 },
+    { name: 'Petya', age: 26 },
+    { name: 'Yoanna', age: 35 },
+    { name: 'Dimitar', age: 36 }
+  ] as User[];
+} 
