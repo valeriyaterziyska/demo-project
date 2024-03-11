b87d173007ac0eb6e2afa93623d8611eecf8e549
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-03-11 13:48:29 +0200

    life cycle methods

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+const IMG_URL = 'https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg';
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.css']
 })
-export class PlayComponent {
+export class PlayComponent implements OnInit, OnDestroy {
   isToggle = false;
   greenBackground = 'background-green';
-  imageUrl = 'https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg';
+  imageUrl = IMG_URL;
+
+  ngOnInit() {
+    console.log('Component Created!');
+  }
+
+  ngAfterViewInit() {
+    console.log('After Init!');
+  }
+
+  ngOnDestroy() {
+    console.log('On Destroy!');
+  }
+
+  ngDoCheck() {
+    console.log({isToggle: this.isToggle});
+    
+  }
 
   handleClick(event: Event) {
     console.log('clicked!', event);
